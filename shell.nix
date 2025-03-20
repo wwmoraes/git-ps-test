9e79aaf06a1643a5a30e60c8c6907a197a363565
William Artero <git@artero.dev>
2025-03-20 10:21:23 +0100

    feat: it is born 🚀

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ system ? builtins.currentSystem
+, sources ? import ./nix/sources.nix
+}: let
+	pkgs = import sources.nixpkgs {
+		inherit system;
+		config.packageOverrides = pkgs: {
+			nur = import sources.NUR { inherit pkgs; };
+			unstable = import sources.unstable { inherit pkgs; };
+		};
+	};
+	inherit (pkgs) lib mkShell;
+in mkShell {
+	packages = [
+		pkgs.git-ps-rs
+	] ++ lib.optionals (builtins.getEnv "CI" != "") [ # CI-only
+	] ++ lib.optionals (builtins.getEnv "CI" == "") [ # local-only
+	];
+}
